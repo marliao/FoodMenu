7bb83443ecafad9e071ed543a1da128f2ab66fd5
1783620271@qq.com <1783620271@qq.com>
2018-12-01 21:37:09 +0800

    ui设计和界面跳转

---
+++ app/src/main/java/com/marliao/foodmenu/Activity/three_Activity.java
@@ -0,0 +1,14 @@
+package com.marliao.foodmenu.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.marliao.foodmenu.R;
+
+public class three_Activity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_menu);
+    }
+}
