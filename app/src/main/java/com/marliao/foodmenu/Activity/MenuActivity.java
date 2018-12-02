3eb6f13edfcccead2a85645eff45d5bda4f3c63b
1783620271@qq.com <1783620271@qq.com>
2018-12-02 13:50:56 +0800

    吴凯凯

--- app/src/main/java/com/marliao/foodmenu/Activity/MenuActivity.java
+++ app/src/main/java/com/marliao/foodmenu/Activity/MenuActivity.java
@@ -34,6 +34,7 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         intiUI();
         intiDate();
+
     }
 
     private void intiDate() {
