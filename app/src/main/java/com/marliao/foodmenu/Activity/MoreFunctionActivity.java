2407a49480e84f4cae67d3a76250fd31fda9aa76
marliao <892564305@qq.com>
2018-12-04 10:10:14 +0800

    李壮

--- app/src/main/java/com/marliao/foodmenu/Activity/MoreFunctionActivity.java
+++ app/src/main/java/com/marliao/foodmenu/Activity/MoreFunctionActivity.java
@@ -2,14 +2,26 @@ package com.marliao.foodmenu.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.marliao.foodmenu.R;
 
 public class MoreFunctionActivity extends AppCompatActivity {
 
+    private TextView tv_user_help;
+    private TextView tv_create_user;
+    private TextView tv_phone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_function);
+        initUI();
+    }
+
+    private void initUI() {
+        tv_user_help = (TextView) findViewById(R.id.tv_user_help);
+        tv_create_user = (TextView) findViewById(R.id.tv_create_user);
+        tv_phone = (TextView) findViewById(R.id.tv_phone);
     }
 }
---
+++ app/src/main/java/com/marliao/foodmenu/Activity/UserHelpActivity.java
@@ -0,0 +1,13 @@
+package com.marliao.foodmenu.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_help);
+    }
+}
